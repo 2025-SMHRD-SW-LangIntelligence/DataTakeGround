1b156c4d7a66ffd129e835169c64ea65078fdc1d
smhrd <smhrd@192.168.219.41>
2025-06-11 12:30:13 +0900

    0611_충돌내기2

--- DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
+++ DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
@@ -8,7 +8,7 @@ public class MemberController {
 		System.out.println("김현식");
 		System.out.println("최연우");
 		System.out.println("한승룡2");
-		System.out.println("송주찬1ㅇㅇㅇㅇ");
+		System.out.println("송주찬1ㅇㅇㅇㅇㅇㅇㅇㅇ");
 		System.out.println("송주찬1");
 		System.out.println("충돌 해결");
 	}
