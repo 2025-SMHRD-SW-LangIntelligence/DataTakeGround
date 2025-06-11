e0da95bbc25c0e1aa3756bc130cac5a6bc16024b
Prizerl <tmdfyd0608@gmail.com>
2025-06-11 15:04:21 +0900

    test

--- DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
+++ DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
@@ -3,22 +3,7 @@ package com.smhrd.dtg.controller;
 public class MemberController {
 
 	public static void main(String[] args) {
-		System.out.println("송주찬");
-		System.out.println("한승룡");
-		System.out.println("김현식");
-		System.out.println("최연우");
-
-		System.out.println("한승룡2");
-		System.out.println("송주찬1ㅇㅇㅇㅇ");
-		System.out.println("송주찬1");
-
-		System.out.println("충돌 해결");
-
-		System.out.println("테스트 1");
-		System.out.println("hs branch");
-		System.out.println("jc branch");
-    	System.out.println("yw brench");
-    	System.out.println("sr branch");
+//		테스트
 
 	}
 }
