6b73b411a9264ced9b23f22a8341751544e90b7e
Prizerl <tmdfyd0608@gmail.com>
2025-06-11 15:11:06 +0900

    branch 테스트

--- DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
+++ DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
@@ -14,7 +14,7 @@ public class MemberController {
 
 		System.out.println("충돌 해결");
 
-		System.out.println("테스트 1");
+		System.out.println("테스트 branch");
 		System.out.println("hs branch");
 		System.out.println("jc branch");
     	System.out.println("yw brench");
