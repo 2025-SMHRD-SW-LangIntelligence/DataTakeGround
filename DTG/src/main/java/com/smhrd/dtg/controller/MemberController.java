97518e09e3236ba662fd9dfcff85776f0de8dd11
Prizerl <tmdfyd0608@gmail.com>
2025-06-11 15:31:33 +0900

    홈페이지

--- DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
+++ DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
@@ -19,8 +19,9 @@ public class MemberController {
 		System.out.println("jc branch");
     	System.out.println("yw brench");
     	System.out.println("sr branch");
-		// 테스트
-		// 테스트2
-    	//asdasd
+		
+    	// 홈페이지
+    	
+    	
 	}
 }
