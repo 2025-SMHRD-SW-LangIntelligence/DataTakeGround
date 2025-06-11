bd70737d4fe9473d8debf62aaf22c552750c0031
smhrd <smhrd@192.168.219.41>
2025-06-11 12:32:26 +0900

    Merge branch 'master' of https://github.com/2025-SMHRD-SW-LangIntelligence/DataTakeGround.git

--- DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
+++ DTG/src/main/java/com/smhrd/dtg/controller/MemberController.java
@@ -10,9 +10,9 @@ public class MemberController {
 		System.out.println("한승룡2");
 		System.out.println("송주찬1ㅇㅇㅇㅇㅇㅇㅇㅇ");
 		System.out.println("송주찬1");
-		System.out.println("충돌 해결");
+		System.out.print("충돌 해결");
 
-		System.out.println("테스트 1");
+		
 
 		
 //		15번 줄
